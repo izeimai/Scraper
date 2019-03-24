cc6572f74cfb4659341c25ef3b483765c6ad48a1
Ize Imai <izeimai@gmail.com>
2019-03-24 11:01:07 -0400

    Added MongoDB URI connection

--- server.js
+++ server.js
@@ -29,8 +29,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Populater";
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
